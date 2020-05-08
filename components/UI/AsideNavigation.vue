96aa4652002e90b96edaf23ae6662cd62db0957e
Simon Konstantyner <simon@konstantyner.dk>
2020-05-08 16:10:43 +0200

    fix title and side navigation

--- components/UI/AsideNavigation.vue
+++ components/UI/AsideNavigation.vue
@@ -3,30 +3,34 @@
     <ul class="space-y-3">
       <li>
         <nuxt-link to="/dashboard">
-          <Icon icon-name="browser-window-open" />Dashboard
+          <Icon icon-name="browser-window-open" class="mr-3" />Dashboard
         </nuxt-link>
       </li>
       <li>
         <nuxt-link to="/clients">
-          <Icon icon-name="user-group" />Clients
+          <Icon icon-name="user-group" class="mr-3" />Clients
         </nuxt-link>
       </li>
       <li>
         <nuxt-link to="/projects">
-          <Icon icon-name="view-carousel" />Projects
+          <Icon icon-name="view-carousel" class="mr-3" />Projects
         </nuxt-link>
       </li>
       <li>
-        <nuxt-link to="/tasks"> <Icon icon-name="checkmark" />Tasks </nuxt-link>
+        <nuxt-link to="/tasks">
+          <Icon icon-name="checkmark" class="mr-3" />Tasks
+        </nuxt-link>
       </li>
       <li class="h-1"></li>
       <li>
         <nuxt-link to="/activities">
-          <Icon icon-name="stroke-width" />Activity
+          <Icon icon-name="view-tile" class="mr-3" />Activity
         </nuxt-link>
       </li>
       <li>
-        <nuxt-link to="/me"> <Icon icon-name="user" />Profile </nuxt-link>
+        <nuxt-link to="/me">
+          <Icon icon-name="user" class="mr-3" />Profile
+        </nuxt-link>
       </li>
     </ul>
   </aside>

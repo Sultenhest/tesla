b068ab0da3b8c44a0571a7a5f3267633a944964b
Simon Konstantyner <simon@konstantyner.dk>
2020-05-11 23:02:42 +0200

    larger icons in aside nav

--- components/UI/AsideNavigation.vue
+++ components/UI/AsideNavigation.vue
@@ -3,33 +3,37 @@
     <ul class="space-y-3">
       <li>
         <nuxt-link to="/dashboard">
-          <Icon icon-name="browser-window-open" class="mr-3" />Dashboard
+          <Icon
+            icon-name="browser-window-open"
+            icon-size="lg"
+            class="mr-3"
+          />Dashboard
         </nuxt-link>
       </li>
       <li>
         <nuxt-link to="/clients">
-          <Icon icon-name="user-group" class="mr-3" />Clients
+          <Icon icon-name="user-group" icon-size="lg" class="mr-3" />Clients
         </nuxt-link>
       </li>
       <li>
         <nuxt-link to="/projects">
-          <Icon icon-name="view-carousel" class="mr-3" />Projects
+          <Icon icon-name="view-carousel" icon-size="lg" class="mr-3" />Projects
         </nuxt-link>
       </li>
       <li>
         <nuxt-link to="/tasks">
-          <Icon icon-name="checkmark" class="mr-3" />Tasks
+          <Icon icon-name="checkmark" icon-size="lg" class="mr-3" />Tasks
         </nuxt-link>
       </li>
       <li class="h-1"></li>
       <li>
         <nuxt-link to="/activities">
-          <Icon icon-name="view-tile" class="mr-3" />Activity
+          <Icon icon-name="view-tile" icon-size="lg" class="mr-3" />Activity
         </nuxt-link>
       </li>
       <li>
         <nuxt-link to="/me">
-          <Icon icon-name="user" class="mr-3" />Profile
+          <Icon icon-name="user" icon-size="lg" class="mr-3" />Profile
         </nuxt-link>
       </li>
     </ul>
